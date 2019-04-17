4685c8bb1c5b50f16a89b77dc904c5687ba43062
Vinicius Araujo <vinicius.brandao.araujo@ccc.ufcg.edu.br>
2019-04-17 17:08:30 -0300

    Template results

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,8 +1,22 @@
-from flask import Flask, session, redirect, url_for, escape, request, render_template
+from flask import Flask, session, redirect, url_for, escape, request, render_template, send_from_directory
 from lime_app.app_back import impl
 import os
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = os.path.join('templates')
+
+__author__ = 'viniaraujoo'
+
+
+app = Flask(__name__,static_folder="data")
+
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+
+file_caminho = os.path.join('WebService-LIME')
+
+
+app.config['UPLOAD_FOLDER'] = os.path.join('WebService-LIME')
+
+
 @app.route('/', methods=['POST'])
 def method_name():
     url1 = request.form.get('model')
@@ -10,9 +24,25 @@ def method_name():
     impl.explanation_image(url1,url2)
     return app.send_static_file("index.hml")
 
+
+
+
+
 @app.route('/tabular', methods=['GET'])
 def render_static():
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'],'fig.jpg')
+    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'fig1.jpg')
+    
+    
+    return render_template("index.html",image_name = 'fig1.jpg')
 
-    return render_template("index.html",user_image = full_filename)
+
+@app.route('/tabular/<filename>')
+def send_image(filename):
+    return send_from_directory("data", filename)
+
+@app.route('/gallery')
+def get_gallery():
+    image_names = os.listdir('./data')
+    print(image_names)
+    return render_template("gallery.html", image_names=image_names)
 
